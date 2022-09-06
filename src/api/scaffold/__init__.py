8e8284239dda1d84abcc5e2aaa7a61ab677092c9
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2022-09-06 09:18:53 +0800

    update: diagnose_task()

--- src/api/scaffold/__init__.py
+++ src/api/scaffold/__init__.py
@@ -22,14 +22,22 @@ def diagnose_task(task_name: typing.Optional[str]) -> typing.Optional[str]:
     """Input detection and normalization"""
     if not task_name or not isinstance(task_name, str) or len(task_name) < 2:
         raise TypeError(f"({task_name})TASK should be string type data")
+
+    # Filename contains illegal characters
     inv = {"\\", "/", ":", "*", "?", "<", ">", "|"}
     if s := set(task_name) & inv:
         raise TypeError(f"({task_name})TASK contains invalid characters({s})")
 
-    task_name = task_name.replace(" ", "_")
-    task_name = task_name.replace(",", "_")
+    # Normalized separator
+    rnv = {",", "-"}
+    for s in rnv:
+        task_name = task_name.replace(s, "_")
+
+    # Convert bad code
     for code in BADCODE:
         task_name.replace(code, BADCODE[code])
 
+    task_name = task_name.strip()
     logger.debug(f"Diagnose task | task_name={task_name}")
+
     return task_name
