adda41c64d02b3b85e9a82a72ef23638aca04b9b
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-09-25 14:54:13 +0800

    Feat(automation): roboflow resnet

--- automation/continue_labeling.py
+++ automation/continue_labeling.py
@@ -6,43 +6,97 @@
 import os
 import shutil
 import sys
+from dataclasses import dataclass, field
 from datetime import datetime
 from pathlib import Path
+from typing import List, Tuple
 
 import hcaptcha_challenger as solver
 
-prompt = "jacket"
-task_name = solver.prompt2task(prompt)
 
-__formats = ("%Y-%m-%d %H:%M:%S.%f", "%Y%m%d%H%M")
-now = datetime.strptime(str(datetime.now()), __formats[0]).strftime(__formats[1])
+@dataclass
+class ContinueLabeling:
+    prompt: str
+    images_dir: Path = field(default=Path)
+    model_path: str = field(default=str)
 
-project_dir = Path(__file__).parent.parent
-from_dir = project_dir.joinpath("database2309", task_name)
-yes_dir = from_dir.joinpath(now, "yes")
-bad_dir = from_dir.joinpath(now, "bad")
+    task_name: str = field(default=str)
+    _images: List[Path] = field(default_factory=list)
 
-images = [
-    from_dir.joinpath(img_name) for img_name in os.listdir(from_dir) if img_name.endswith(".png")
-]
+    def __post_init__(self):
+        self._images = [
+            self.images_dir.joinpath(img_name)
+            for img_name in os.listdir(self.images_dir)
+            if img_name.endswith(".png")
+        ]
 
-if not images:
-    sys.exit()
+    @classmethod
+    def from_prompt(cls, prompt: str, from_dir: Path, model_dir: Path | None = None, **kwargs):
+        prompt = prompt.replace("_", " ")
 
-solver.install(upgrade=True)
+        # Select: model name
+        task_name = solver.prompt2task(prompt)
 
-yes_dir.mkdir(parents=True, exist_ok=True)
-bad_dir.mkdir(parents=True, exist_ok=True)
+        # Match: image database
+        images_dir = from_dir.joinpath(task_name)
+        if not images_dir.exists():
+            raise FileNotFoundError(f"NOT Found Images_dir - {images_dir=}")
 
-classifier = solver.BinaryClassifier()
-model_path = project_dir.joinpath("model", task_name, f"{task_name}.onnx")
+        # Match: local model
+        model_path = ""
+        if model_dir and isinstance(model_dir, Path):
+            model_path = model_dir.joinpath(task_name, f"{task_name}.onnx")
+            if not model_path.exists():
+                print("NOT Found Model_Path, switch to remote model")
+            else:
+                model_path = str(model_path)
 
-results = classifier.execute(prompt, images, model_path=str(model_path))
-for i, result in enumerate(results):
-    if result is True:
-        shutil.move(images[i], yes_dir)
-    elif result is False:
-        shutil.move(images[i], bad_dir)
+        return cls(
+            prompt=prompt,
+            task_name=task_name,
+            images_dir=images_dir,
+            model_path=model_path,
+            **kwargs,
+        )
 
-if "win32" in sys.platform:
-    os.startfile(from_dir)
+    def mkdir(self) -> Tuple[Path, Path]:
+        __formats = ("%Y-%m-%d %H:%M:%S.%f", "%Y%m%d%H%M")
+        now = datetime.strptime(str(datetime.now()), __formats[0]).strftime(__formats[1])
+        yes_dir = self.images_dir.joinpath(now, "yes")
+        bad_dir = self.images_dir.joinpath(now, "bad")
+        yes_dir.mkdir(parents=True, exist_ok=True)
+        bad_dir.mkdir(parents=True, exist_ok=True)
+
+        return yes_dir, bad_dir
+
+    def execute(self):
+        if not self._images:
+            sys.exit()
+        if self.model_path == "":
+            solver.install(upgrade=True)
+
+        yes_dir, bad_dir = self.mkdir()
+
+        classifier = solver.BinaryClassifier()
+        results = classifier.execute(self.prompt, self._images, model_path=self.model_path)
+        for i, result in enumerate(results):
+            if result is True:
+                shutil.move(self._images[i], yes_dir)
+            elif result is False:
+                shutil.move(self._images[i], bad_dir)
+
+        if "win32" in sys.platform:
+            os.startfile(self.images_dir)
+
+
+def run(prompt: str):
+    project_dir = Path(__file__).parent.parent
+    from_dir = project_dir.joinpath("database2309")
+    model_dir = project_dir.joinpath("model")
+
+    cl = ContinueLabeling.from_prompt(prompt, from_dir, model_dir)
+    cl.execute()
+
+
+if __name__ == "__main__":
+    run(prompt="robot")
